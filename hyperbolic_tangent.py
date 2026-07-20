710c6b07f7fe814aa32f75dbbcad39f005132906
Josue Guzman <sleepyy18th@gmail.com>
2026-07-20 00:23:59 -0600

    Basic implementation

---
+++ hyperbolic_tangent.py
@@ -0,0 +1,8 @@
+import numpy as np
+from activation_layer import Activation
+
+class Tanh(Activation):
+    def __init__(self):
+        tanh = lambda x: np.tanh(x)
+        tanh_prime = lambda x: 1 - np.tanh(x) ** 2
+        super().__init__(tanh, tanh_prime)
